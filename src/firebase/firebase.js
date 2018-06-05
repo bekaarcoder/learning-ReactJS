0a852cb4b2c69bbf828ade84db30149f01b83c96
Shashank <shashank_smaty@yahoo.co.in>
2018-06-05 07:57:47 +0530

    Added firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,54 @@ const config = {
 
 firebase.initializeApp(config);
 
-firebase.database().ref('users').set({
-	username: 'shashank'
-});
+const database = firebase.database();
+
+export { firebase, database as default };
+
+/*database.ref('users').set({
+	username: 'shashank',
+	age: 24,
+	city: 'New Delhi'
+});*/
+
+
+// database.ref('users').set(user);
+
+/*database.ref('users/city').remove()
+	.then(() => {
+		console.log("Removed Data!");
+	}).catch((e) => {
+		console.log("Failed: ", e.message);
+	});*/
+
+/*database.ref('expenses').push({
+	description: 'Gas Bill',
+	note: '',
+	amount: 1000,
+	createdAt: 123432123
+});*/
+
+/*database.ref('expenses').once('value')
+	.then((dataSnapshot) => {
+		const expenses = [];
+		dataSnapshot.forEach((childSnapshot) => {
+			expenses.push({
+				id: childSnapshot.key,
+				...childSnapshot.val()
+			});
+		});
+		console.log(expenses);
+	}).catch((e) => {
+		console.log(e);
+	});
+
+database.ref('expenses').on('value', (dataSnapshot) => {
+	const expenses = [];
+		dataSnapshot.forEach((childSnapshot) => {
+			expenses.push({
+				id: childSnapshot.key,
+				...childSnapshot.val()
+			});
+		});
+		console.log(expenses);
+	});*/
